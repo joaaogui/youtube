10858f854fb7f6d41dd3e2a6fca15108b3924a49
João Guilherme <joaaogui@gmail.com>
2023-05-02 21:11:19 -0300

    fix: adds improvements

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import vuetify from './plugins/vuetify';
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import vuetify from "./plugins/vuetify";
+import { inject } from "@vercel/analytics";
+inject();
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
